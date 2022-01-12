a42b0841163807d00fd64ef9d5f81561d24d3998
snpravda <sn.pravda@gmail.com>
2022-01-12 20:43:39 +0200

    added google sign in

--- android_app/app/src/main/java/com/example/mapwithmarker/MainActivity.kt
+++ android_app/app/src/main/java/com/example/mapwithmarker/MainActivity.kt
@@ -1,25 +1,57 @@
 package com.example.mapwithmarker
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+//import kotlinx.android.synthetic.main.activity_main.*
 
 var server = "https://jsonplaceholder.typicode.com/"
 
 class MainActivity : AppCompatActivity() {
     private lateinit var getScooterButton: Button
+    private lateinit var sign_out_button: Button
+
+    companion object {
+        fun getLaunchIntent(from: Context) = Intent(from, MainActivity::class.java).apply {
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupUI()
 
         getScooterButton = findViewById(R.id.get_scooters_btn)
         getScooterButton.setOnClickListener {
             this.getScooters()
         }
+    }
+
+    private fun setupUI() {
+        sign_out_button = findViewById(R.id.sign_out_button)
+        sign_out_button.setOnClickListener {
+            signOut()
+        }
+    }
+
+    private fun signOut() {
+        FirebaseAuth.getInstance().signOut()
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(refresh_token)
+            .requestEmail()
+            .build()
+
+        val googleSignInClient = GoogleSignIn.getClient(this, gso)
+        googleSignInClient.signOut()
+        startActivity(SignInActivity.getLaunchIntent(this))
     }
 
     private fun getScooters () {
---
+++ android_app/app/src/main/java/com/example/mapwithmarker/SignInActivity.kt
@@ -0,0 +1,99 @@
+package com.example.mapwithmarker
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+
+val refresh_token = "272963677900-n441g6ql982ckt37jlqil37m9hdc2uqq.apps.googleusercontent.com"
+
+class SignInActivity : AppCompatActivity() {
+
+    val RC_SIGN_IN: Int = 1
+    lateinit var mGoogleSignInClient: GoogleSignInClient
+    lateinit var mGoogleSignInOptions: GoogleSignInOptions
+
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var google_button: SignInButton
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+        configureGoogleSignIn()
+        setupUI()
+        firebaseAuth = FirebaseAuth.getInstance()
+    }
+
+    companion object {
+        fun getLaunchIntent(from: Context) = Intent(from, SignInActivity::class.java).apply {
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val user = FirebaseAuth.getInstance().currentUser
+        if (user != null) {
+            startActivity(MainActivity.getLaunchIntent(this))
+            finish()
+        }
+    }
+
+    private fun configureGoogleSignIn() {
+        mGoogleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(refresh_token)
+            .requestEmail()
+            .build()
+        mGoogleSignInClient = GoogleSignIn.getClient(this, mGoogleSignInOptions)
+    }
+
+    private fun setupUI() {
+        google_button = findViewById(R.id.google_button)
+        google_button.setOnClickListener {
+            signIn()
+        }
+    }
+
+    private fun signIn() {
+        val signInIntent: Intent = mGoogleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+//            try {
+            val account : GoogleSignInAccount? = task.getResult(ApiException::class.java)
+            firebaseAuthWithGoogle(account!!)
+//            } catch (e: ApiException) {
+//                Toast.makeText(this, "Google sign in failed:(", Toast.LENGTH_LONG).show()
+//            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
+        firebaseAuth.signInWithCredential(credential).addOnCompleteListener {
+            if (it.isSuccessful) {
+
+                startActivity(MainActivity.getLaunchIntent(this))
+            } else {
+                Toast.makeText(this, "Google sign in failed:(", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+
+}
